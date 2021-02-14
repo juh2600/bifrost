158d4af096cfad347c7b96b7d4473f286dae44f2
Joe Reed <josephreed2600@gmail.com>
2021-02-13 19:58:49 -0700

    organize :/db/

--- db/db-connect.js
+++ db/db-connect.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+const logger = require('logger').get('db');
 const cass = require('cassandra-driver');
 const secrets = require('../secrets').cassandra;
 
@@ -12,4 +13,6 @@ const client = new cass.Client({
 	}
 });
 
-client.connect().then((x) => console.log(x, 'yeet'));
+client.connect().then(() => logger.info(`Connected to ${secrets.host}`));
+
+module.exports = client;

f6ad3ec861ab4d9591a90b500908b6e2ee4cf46d
John <jashby@student.neumont.edu>
2021-02-13 21:48:46 -0700

    Sign-Up Page - Draft

--- routes/frontend.js
+++ routes/frontend.js
@@ -1,40 +1,61 @@
-const logger = require('logger').get('frontend');
+const logger = require("logger").get("frontend");
 
 const index = (req, res) => {
-	res.render('index', {
-		// key: value
-	});
+  res.render("index", {
+    // key: value
+  });
 };
 
 const app = (req, res) => {
-	res.render('app', {
-		// key: value
-	});
+  res.render("app", {
+    // key: value
+  });
 };
+
+const signUp = (req, res) => {
+  res.render("signUp", {
+    // key: value
+  });
+};
+
+const logIn = (req, res) => {
+  res.render("logIn", {
+    // key: value
+  });
+};
+
 const createGuild = (req, res) => {
-	res.render('createGuild', {
-		// key: value
-	});
+  res.render("createGuild", {
+    // key: value
+  });
 };
 
 const routes = [
-	{
-		uri: '/',
-		methods: ['get'],
-		handler: index
-	},
-	{
-		uri: '/app',
-		methods: ['get'],
-		handler: app
-	},
-	{
-		uri: '/guilds/create',
-		methods: ['get'],
-		handler: createGuild
-	}
-
+  {
+    uri: "/",
+    methods: ["get"],
+    handler: index,
+  },
+  {
+    uri: "/app",
+    methods: ["get"],
+    handler: app,
+  },
+  {
+    uri: "/signUp",
+    methods: ["get"],
+    handler: signUp,
+  },
+  {
+    uri: "/logIn",
+    methods: ["get"],
+    handler: logIn,
+  },
+  {
+    uri: "/guilds/create",
+    methods: ["get"],
+    handler: createGuild,
+  },
 ];
 
-
-module.exports = { logger, routes }
+module.exports = { logger, routes };

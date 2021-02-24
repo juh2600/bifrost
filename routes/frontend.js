151a7893e07f7d9327497bc52b518cf11a56766d
RUnroe <ryanunroe@gmail.com>
2021-02-23 21:07:07 -0700

    switch to dal

--- routes/frontend.js
+++ routes/frontend.js
@@ -1,91 +1,18 @@
 const logger = require("logger").get("frontend");
 
-const dal = {
-  getGuilds: async () => {
-    return [
-      {
-        guild_id: "456",
-        icon_id: "sdfsdfjdshfkjashfkjahdsflkjadsa",
-        name: "a"
-      },
-      {
-        guild_id: "123",
-        icon_id: "qwertyuiop",
-        name: "b"
-      },
-      {
-        guild_id: "4124",
-        icon_id: "sdfsdfjdshfkjashfkjahdsflkjadsa",
-        name: "c"
-      },
-      {
-        guild_id: "452345",
-        icon_id: "qwertyuiop",
-        name: "d"
-      },
-      {
-        guild_id: "75",
-        icon_id: "sdfsdfjdshfkjashfkjahdsflkjadsa",
-        name: "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
-      },
-      {
-        guild_id: "45",
-        icon_id: "qwertyuiop",
-        name: "LONG NAMEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
-      },
-      {
-        guild_id: "87654",
-        icon_id: "sdfsdfjdshfkjashfkjahdsflkjadsa",
-        name: "e"
-      },
-      {
-        guild_id: "4536",
-        icon_id: "qwertyuiop",
-        name: "f"
-      },
-      {
-        guild_id: "2142",
-        icon_id: "sdfsdfjdshfkjashfkjahdsflkjadsa",
-        name: "g"
-      },
-      {
-        guild_id: "8798",
-        icon_id: "qwertyuiop",
-        name: "h"
-      },
-      {
-        guild_id: "09",
-        icon_id: "sdfsdfjdshfkjashfkjahdsflkjadsa",
-        name: "I"
-      },
-      {
-        guild_id: "345678",
-        icon_id: "qwertyuiop",
-        name: "J"
-      }
-    ]
-  },
-  getUsers: async() => {
-    return [
-		  {"user_id": "811410903487692800", "name": "8888888888888888888888888888888888888888888888888888888888888888", "icon_id": "811411148556681216", "email": "josephreed2600@gmail.com", "discriminator": 1234}
-		, {"user_id": "811411148556681216", "name": "Joe 2", "icon_id": "811411487263506432", "email": "josephreed2601@gmail.com", "discriminator": 1234}
-		, {"user_id": "811411322687406080", "name": "Joe 3", "icon_id": "811411461028134912", "email": "josephreed2602@gmail.com", "discriminator": 1234}
-		, {"user_id": "811411348876640256", "name": "Joe 4", "icon_id": "811411438727020544", "email": "josephreed2603@gmail.com", "discriminator": 1234}
-		, {"user_id": "811411377200775168", "name": "Joe 5", "icon_id": "811411410646155264", "email": "josephreed2604@gmail.com", "discriminator": 1234}
-    , {"user_id": "811410903487692808", "name": "Joe 1", "icon_id": "811411148556681216", "email": "josephreed2600@gmail.com", "discriminator": 1234}
-		, {"user_id": "811411148556681216", "name": "Joe 2", "icon_id": "811411487263506432", "email": "josephreed2601@gmail.com", "discriminator": 1234}
-		, {"user_id": "811411322687406080", "name": "Joe 3", "icon_id": "811411461028134912", "email": "josephreed2602@gmail.com", "discriminator": 1234}
-		, {"user_id": "811411348876640256", "name": "Joe 4", "icon_id": "811411438727020544", "email": "josephreed2603@gmail.com", "discriminator": 1234}
-		, {"user_id": "811411377200775168", "name": "Joe 5", "icon_id": "811411410646155264", "email": "josephreed2604@gmail.com", "discriminator": 1234}
-    , {"user_id": "811410903487692806", "name": "Joe 1", "icon_id": "811411148556681216", "email": "josephreed2600@gmail.com", "discriminator": 1234}
-		, {"user_id": "811411148556681216", "name": "Joe 2", "icon_id": "811411487263506432", "email": "josephreed2601@gmail.com", "discriminator": 1234}
-		, {"user_id": "811411322687406080", "name": "Joe 3", "icon_id": "811411461028134912", "email": "josephreed2602@gmail.com", "discriminator": 1234}
-		, {"user_id": "811411348876640256", "name": "Joe 4", "icon_id": "811411438727020544", "email": "josephreed2603@gmail.com", "discriminator": 1234}
-		, {"user_id": "811411377200775168", "name": "Joe 5", "icon_id": "811411410646155264", "email": "josephreed2604@gmail.com", "discriminator": 1234}
-	];
-  }
-}
+let db;
+const configure = (obj) => {
+	db = obj['db'];
+	console.log(obj, db);
+};
 
+const handle = (code, req, res) => {
+	return errors => {
+		res
+			.status(code)
+			.json(errors)
+	};
+};
 
 const index = (req, res) => {
   res.render("index", {
@@ -94,11 +21,11 @@ const index = (req, res) => {
 };
 
 const app = (req, res) => {
-  dal.getGuilds().then(dalGuildList => {
-    dal.getUsers().then(dalUsersList => {
+  db.getGuilds().then(dbGuildList => {
+    db.getUsers().then(dbUsersList => {
       res.render("app", {
-        guildList: dalGuildList,
-        userList: dalUsersList
+        guildList: dbGuildList,
+        userList: dbUsersList
       });
     });
   });
@@ -180,4 +107,4 @@ const routes = [
 	}
 ];
 
-module.exports = { logger, routes };
+module.exports = { logger, routes, configure };

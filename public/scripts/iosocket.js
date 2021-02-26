3c2f574b66d029056e6e46113fb08b0cfbe60736
RUnroe <ryanunroe@gmail.com>
2021-02-26 12:41:39 -0700

    Make chat area scrollable

--- public/scripts/iosocket.js
+++ public/scripts/iosocket.js
@@ -1,6 +1,6 @@
 const socket = io();
 
-const chatAreaId = document.getElementById("chatArea");
+const chatAreaId = document.getElementsByClassName("chat-area")[0];
 
 //TODO: Send message to dal. dal sends message to db. I lost my train of thot
 // done! -- jtreed

81f769426e30842cd195616032d724983fbb2948
RUnroe <ryanunroe@gmail.com>
2021-03-01 19:14:16 -0700

    Allow users to press escape to leave settings page

--- public/scripts/appSettings.js
+++ public/scripts/appSettings.js
@@ -1,5 +1,12 @@
 
 const setupCancelEventListeners = () => {
+    document.addEventListener("keyup", (evt) => {
+        if (evt.key === 'Escape') {
+          evt.preventDefault();
+          window.location.href = "/app";
+        }
+    });
+
     document.querySelectorAll('.cancel').forEach(button => {
         button.addEventListener("click", () => {
             window.location.href = "/app";

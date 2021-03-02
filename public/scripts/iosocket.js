1eeeac82cd39398513a6ad13a0ecb0324e621ee8
RUnroe <ryanunroe@gmail.com>
2021-03-01 19:16:31 -0700

    Prevent users from sending empty messages

--- public/scripts/iosocket.js
+++ public/scripts/iosocket.js
@@ -15,13 +15,16 @@ messageInput.addEventListener("keypress", (evt) => {
   if (evt.key === 'Enter') {
     evt.preventDefault();
     let msg = messageInput.value;
-    socket.emit("chatMessage", {
-      channel_id: selectedChannelId,
-			author_id: document.getElementById('currentUser').dataset.userId,
-      body: msg
-    });
+    //Only allow user to send message if there is a message
+    if(msg){
+      socket.emit("chatMessage", {
+        channel_id: selectedChannelId,
+        author_id: document.getElementById('currentUser').dataset.userId,
+        body: msg
+      });
 
-    //Clear input field after message is sent
-    messageInput.value = "";
+      //Clear input field after message is sent
+      messageInput.value = "";
+    }
   }
 });
